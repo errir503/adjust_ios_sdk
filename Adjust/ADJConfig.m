briankracoff/ios_sdk

Log supressing level

Adjust/ADJConfig.m
@@ -10,39 +10,55 @@
 #import "ADJAdjustFactory.h"
 #import "ADJLogger.h"
 #import "ADJUtil.h"
+#import "Adjust.h"
+
+@interface ADJConfig()
+
+@property (nonatomic, weak) id<ADJLogger> logger;
+@property (nonatomic, assign) BOOL allowSupressLogLevel;
+
+@end
 
 @implementation ADJConfig
 
-+ (ADJConfig *) configWithAppToken:(NSString *)appToken
-                       environment:(NSString *)environment {
++ (ADJConfig *)configWithAppToken:(NSString *)appToken
+                      environment:(NSString *)environment {
     return [[ADJConfig alloc] initWithAppToken:appToken environment:environment];
 }
 
-- (id) initWithAppToken:(NSString *)appToken
-            environment:(NSString *)environment
++ (ADJConfig *)configWithAppToken:(NSString *)appToken
+                      environment:(NSString *)environment
+             allowSupressLogLevel:(BOOL)allowSupressLogLevel
 {
-    if (![self checkAppToken:appToken]) return self;
-    if (![self checkEnvironment:environment]) return self;
-
-    return [self initSelfWithAppToken:appToken environment:environment];
+    return [[ADJConfig alloc] initWithAppToken:appToken environment:environment allowSupressLogLevel:allowSupressLogLevel];
 }
 
-- (id) initWithoutCheckAppToken:(NSString *)appToken
-                    environment:(NSString *)environment
+- (id)initWithAppToken:(NSString *)appToken
+           environment:(NSString *)environment
+{
+    return [self initWithAppToken:appToken
+                      environment:environment
+             allowSupressLogLevel:NO];
+}
+
+- (id)initWithAppToken:(NSString *)appToken
+           environment:(NSString *)environment
+  allowSupressLogLevel:(BOOL)allowSupressLogLevel
 {
     self = [super init];
     if (self == nil) return nil;
 
-    return [self initSelfWithAppToken:appToken environment:environment];
-}
+    self.allowSupressLogLevel = allowSupressLogLevel;
+    self.logger = ADJAdjustFactory.logger;
+    // default values
+    [self setLogLevel:ADJLogLevelInfo];
 
-- (id) initSelfWithAppToken:(NSString *)appToken
-                environment:(NSString *)environment {
+    if (![self checkEnvironment:environment]) return self;
+    if (![self checkAppToken:appToken]) return self;
+
     _appToken = appToken;
     _environment = environment;
-
     // default values
-    self.logLevel = ADJLogLevelInfo;
     _hasResponseDelegate = NO;
     _hasAttributionChangedDelegate = NO;
     self.eventBufferingEnabled = NO;
@@ -50,59 +66,85 @@
     return self;
 }
 
-- (void) setDelegate:(NSObject<AdjustDelegate> *)delegate {
+- (void)setLogLevel:(ADJLogLevel)logLevel {
+    [self setLogLevel:logLevel environment:self.environment];
+}
+
+- (void)setLogLevelWithEnvironment:(NSString *)environment {
+    [self setLogLevel:self.logLevel environment:environment];
+}
+
+- (void)setLogLevel:(ADJLogLevel)logLevel
+        environment:(NSString *)environment{
+    if ([environment isEqualToString:ADJEnvironmentProduction]) {
+        if (self.allowSupressLogLevel) {
+            _logLevel = ADJLogLevelSupress;
+        } else {
+            _logLevel = ADJLogLevelAssert;
+        }
+    } else {
+        if (!self.allowSupressLogLevel &&
+            _logLevel == ADJLogLevelSupress) {
+            _logLevel = ADJLogLevelAssert;
+        } else {
+            _logLevel = logLevel;
+        }
+    }
+    [self.logger setLogLevel:self.logLevel];
+}
+
+
+- (void)setDelegate:(NSObject<AdjustDelegate> *)delegate {
     _hasResponseDelegate = NO;
     _hasAttributionChangedDelegate = NO;
     BOOL implementsDeeplinkCallback = NO;
 
-    id<ADJLogger> logger = ADJAdjustFactory.logger;
-
     if ([ADJUtil isNull:delegate]) {
-        [logger warn:@"Delegate is nil"];
+        [self.logger warn:@"Delegate is nil"];
         _delegate = nil;
         return;
     }
 
     if ([delegate respondsToSelector:@selector(adjustAttributionChanged:)]) {
-        [logger debug:@"Delegate implements adjustAttributionChanged:"];
+        [self.logger debug:@"Delegate implements adjustAttributionChanged:"];
 
         _hasResponseDelegate = YES;
         _hasAttributionChangedDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustEventTrackingSucceeded:)]) {
-        [logger debug:@"Delegate implements adjustEventTrackingSucceeded:"];
+        [self.logger debug:@"Delegate implements adjustEventTrackingSucceeded:"];
 
         _hasResponseDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustEventTrackingFailed:)]) {
-        [logger debug:@"Delegate implements adjustEventTrackingFailed:"];
+        [self.logger debug:@"Delegate implements adjustEventTrackingFailed:"];
 
         _hasResponseDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustSessionTrackingSucceeded:)]) {
-        [logger debug:@"Delegate implements adjustSessionTrackingSucceeded:"];
+        [self.logger debug:@"Delegate implements adjustSessionTrackingSucceeded:"];
 
         _hasResponseDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustSessionTrackingFailed:)]) {
-        [logger debug:@"Delegate implements adjustSessionTrackingFailed:"];
+        [self.logger debug:@"Delegate implements adjustSessionTrackingFailed:"];
 
         _hasResponseDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustDeeplinkResponse:)]) {
-        [logger debug:@"Delegate implements adjustDeeplinkResponse:"];
+        [self.logger debug:@"Delegate implements adjustDeeplinkResponse:"];
 
         // does not enable hasDelegate flag
         implementsDeeplinkCallback = YES;
     }
 
     if (!(self.hasResponseDelegate || implementsDeeplinkCallback)) {
-        [logger error:@"Delegate does not implement any optional method"];
+        [self.logger error:@"Delegate does not implement any optional method"];
         _delegate = nil;
         return;
     }
@@ -110,37 +152,38 @@
     _delegate = delegate;
 }
 
-- (BOOL) checkEnvironment:(NSString *)environment
+- (BOOL)checkEnvironment:(NSString *)environment
 {
-    id<ADJLogger> logger = ADJAdjustFactory.logger;
     if ([ADJUtil isNull:environment]) {
-        [logger error:@"Missing environment"];
+        [self.logger error:@"Missing environment"];
         return NO;
     }
     if ([environment isEqualToString:ADJEnvironmentSandbox]) {
-        [logger assert:@"SANDBOX: Adjust is running in Sandbox mode. Use this setting for testing. Don't forget to set the environment to `production` before publishing"];
+        [self setLogLevelWithEnvironment:environment];
+        [self.logger assert:@"SANDBOX: Adjust is running in Sandbox mode. Use this setting for testing. Don't forget to set the environment to `production` before publishing"];
         return YES;
     } else if ([environment isEqualToString:ADJEnvironmentProduction]) {
-        [logger assert:@"PRODUCTION: Adjust is running in Production mode. Use this setting only for the build that you want to publish. Set the environment to `sandbox` if you want to test your app!"];
+        [self setLogLevelWithEnvironment:environment];
+        [self.logger assert:@"PRODUCTION: Adjust is running in Production mode. Use this setting only for the build that you want to publish. Set the environment to `sandbox` if you want to test your app!"];
         return YES;
     }
-    [logger error:@"Unknown environment '%@'", environment];
+    [self.logger error:@"Unknown environment '%@'", environment];
     return NO;
 }
 
 - (BOOL)checkAppToken:(NSString *)appToken {
     if ([ADJUtil isNull:appToken]) {
-        [ADJAdjustFactory.logger error:@"Missing App Token"];
+        [self.logger error:@"Missing App Token"];
         return NO;
     }
     if (appToken.length != 12) {
-        [ADJAdjustFactory.logger error:@"Malformed App Token '%@'", appToken];
+        [self.logger error:@"Malformed App Token '%@'", appToken];
         return NO;
     }
     return YES;
 }
 
-- (BOOL) isValid {
+- (BOOL)isValid {
     return self.appToken != nil;
 }
 

errir503/adjust_ios_sdk

Adjust/ADJConfig.m
@@ -10,39 +10,55 @@
 #import "ADJAdjustFactory.h"
 #import "ADJLogger.h"
 #import "ADJUtil.h"
+#import "Adjust.h"
+
+@interface ADJConfig()
+
+@property (nonatomic, weak) id<ADJLogger> logger;
+@property (nonatomic, assign) BOOL allowSupressLogLevel;
+
+@end
 
 @implementation ADJConfig
 
-+ (ADJConfig *) configWithAppToken:(NSString *)appToken
-                       environment:(NSString *)environment {
++ (ADJConfig *)configWithAppToken:(NSString *)appToken
+                      environment:(NSString *)environment {
     return [[ADJConfig alloc] initWithAppToken:appToken environment:environment];
 }
 
-- (id) initWithAppToken:(NSString *)appToken
-            environment:(NSString *)environment
++ (ADJConfig *)configWithAppToken:(NSString *)appToken
+                      environment:(NSString *)environment
+             allowSupressLogLevel:(BOOL)allowSupressLogLevel
 {
-    if (![self checkAppToken:appToken]) return self;
-    if (![self checkEnvironment:environment]) return self;
-
-    return [self initSelfWithAppToken:appToken environment:environment];
+    return [[ADJConfig alloc] initWithAppToken:appToken environment:environment allowSupressLogLevel:allowSupressLogLevel];
 }
 
-- (id) initWithoutCheckAppToken:(NSString *)appToken
-                    environment:(NSString *)environment
+- (id)initWithAppToken:(NSString *)appToken
+           environment:(NSString *)environment
+{
+    return [self initWithAppToken:appToken
+                      environment:environment
+             allowSupressLogLevel:NO];
+}
+
+- (id)initWithAppToken:(NSString *)appToken
+           environment:(NSString *)environment
+  allowSupressLogLevel:(BOOL)allowSupressLogLevel
 {
     self = [super init];
     if (self == nil) return nil;
 
-    return [self initSelfWithAppToken:appToken environment:environment];
-}
+    self.allowSupressLogLevel = allowSupressLogLevel;
+    self.logger = ADJAdjustFactory.logger;
+    // default values
+    [self setLogLevel:ADJLogLevelInfo environment:environment];
 
-- (id) initSelfWithAppToken:(NSString *)appToken
-                environment:(NSString *)environment {
+    if (![self checkEnvironment:environment]) return self;
+    if (![self checkAppToken:appToken]) return self;
+
     _appToken = appToken;
     _environment = environment;
-
     // default values
-    self.logLevel = ADJLogLevelInfo;
     _hasResponseDelegate = NO;
     _hasAttributionChangedDelegate = NO;
     self.eventBufferingEnabled = NO;
@@ -50,59 +66,81 @@
     return self;
 }
 
-- (void) setDelegate:(NSObject<AdjustDelegate> *)delegate {
+- (void)setLogLevel:(ADJLogLevel)logLevel {
+    [self setLogLevel:logLevel environment:self.environment];
+}
+
+- (void)setLogLevel:(ADJLogLevel)logLevel
+        environment:(NSString *)environment{
+    if ([environment isEqualToString:ADJEnvironmentProduction]) {
+        if (self.allowSupressLogLevel) {
+            _logLevel = ADJLogLevelSupress;
+        } else {
+            _logLevel = ADJLogLevelAssert;
+        }
+    } else {
+        if (!self.allowSupressLogLevel &&
+            logLevel == ADJLogLevelSupress) {
+            _logLevel = ADJLogLevelAssert;
+        } else {
+            _logLevel = logLevel;
+        }
+    }
+    [self.logger setLogLevel:self.logLevel];
+}
+
+
+- (void)setDelegate:(NSObject<AdjustDelegate> *)delegate {
     _hasResponseDelegate = NO;
     _hasAttributionChangedDelegate = NO;
     BOOL implementsDeeplinkCallback = NO;
 
-    id<ADJLogger> logger = ADJAdjustFactory.logger;
-
     if ([ADJUtil isNull:delegate]) {
-        [logger warn:@"Delegate is nil"];
+        [self.logger warn:@"Delegate is nil"];
         _delegate = nil;
         return;
     }
 
     if ([delegate respondsToSelector:@selector(adjustAttributionChanged:)]) {
-        [logger debug:@"Delegate implements adjustAttributionChanged:"];
+        [self.logger debug:@"Delegate implements adjustAttributionChanged:"];
 
         _hasResponseDelegate = YES;
         _hasAttributionChangedDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustEventTrackingSucceeded:)]) {
-        [logger debug:@"Delegate implements adjustEventTrackingSucceeded:"];
+        [self.logger debug:@"Delegate implements adjustEventTrackingSucceeded:"];
 
         _hasResponseDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustEventTrackingFailed:)]) {
-        [logger debug:@"Delegate implements adjustEventTrackingFailed:"];
+        [self.logger debug:@"Delegate implements adjustEventTrackingFailed:"];
 
         _hasResponseDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustSessionTrackingSucceeded:)]) {
-        [logger debug:@"Delegate implements adjustSessionTrackingSucceeded:"];
+        [self.logger debug:@"Delegate implements adjustSessionTrackingSucceeded:"];
 
         _hasResponseDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustSessionTrackingFailed:)]) {
-        [logger debug:@"Delegate implements adjustSessionTrackingFailed:"];
+        [self.logger debug:@"Delegate implements adjustSessionTrackingFailed:"];
 
         _hasResponseDelegate = YES;
     }
 
     if ([delegate respondsToSelector:@selector(adjustDeeplinkResponse:)]) {
-        [logger debug:@"Delegate implements adjustDeeplinkResponse:"];
+        [self.logger debug:@"Delegate implements adjustDeeplinkResponse:"];
 
         // does not enable hasDelegate flag
         implementsDeeplinkCallback = YES;
     }
 
     if (!(self.hasResponseDelegate || implementsDeeplinkCallback)) {
-        [logger error:@"Delegate does not implement any optional method"];
+        [self.logger error:@"Delegate does not implement any optional method"];
         _delegate = nil;
         return;
     }
@@ -110,37 +148,36 @@
     _delegate = delegate;
 }
 
-- (BOOL) checkEnvironment:(NSString *)environment
+- (BOOL)checkEnvironment:(NSString *)environment
 {
-    id<ADJLogger> logger = ADJAdjustFactory.logger;
     if ([ADJUtil isNull:environment]) {
-        [logger error:@"Missing environment"];
+        [self.logger error:@"Missing environment"];
         return NO;
     }
     if ([environment isEqualToString:ADJEnvironmentSandbox]) {
-        [logger assert:@"SANDBOX: Adjust is running in Sandbox mode. Use this setting for testing. Don't forget to set the environment to `production` before publishing"];
+        [self.logger assert:@"SANDBOX: Adjust is running in Sandbox mode. Use this setting for testing. Don't forget to set the environment to `production` before publishing"];
         return YES;
     } else if ([environment isEqualToString:ADJEnvironmentProduction]) {
-        [logger assert:@"PRODUCTION: Adjust is running in Production mode. Use this setting only for the build that you want to publish. Set the environment to `sandbox` if you want to test your app!"];
+        [self.logger assert:@"PRODUCTION: Adjust is running in Production mode. Use this setting only for the build that you want to publish. Set the environment to `sandbox` if you want to test your app!"];
         return YES;
     }
-    [logger error:@"Unknown environment '%@'", environment];
+    [self.logger error:@"Unknown environment '%@'", environment];
     return NO;
 }
 
 - (BOOL)checkAppToken:(NSString *)appToken {
     if ([ADJUtil isNull:appToken]) {
-        [ADJAdjustFactory.logger error:@"Missing App Token"];
+        [self.logger error:@"Missing App Token"];
         return NO;
     }
     if (appToken.length != 12) {
-        [ADJAdjustFactory.logger error:@"Malformed App Token '%@'", appToken];
+        [self.logger error:@"Malformed App Token '%@'", appToken];
         return NO;
     }
     return YES;
 }
 
-- (BOOL) isValid {
+- (BOOL)isValid {
     return self.appToken != nil;
 }
 